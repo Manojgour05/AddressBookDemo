e3fcef63095e68703e4b566039f9de995381c296
Manojgour05 <manojgour6811@gmail.com>
2020-10-19 09:01:09 +0530

    Added:File added

---
+++ AddressBook2.java
@@ -0,0 +1,8 @@
+package com.nt.addressBook;
+
+import java.util.Scanner;
+public class AddressBook2{
+    public static void main(String[] args){
+    	System.out.println("welcome to Display addressbook ");
+    }
+}
